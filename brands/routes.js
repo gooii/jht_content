dbefa8f5a4b1ec778cdd7a3a1ccfeb7bab11cf25
paulflieshman <paul.flieshman@jisc.ac.uk>
2016-05-09 15:11:06 +0100

    fixed typo for locallondon feature in routes.js file

--- brands/routes.js
+++ brands/routes.js
@@ -20,6 +20,6 @@ var routes = {
   "shakespeare"           : "features/shakespeare.html",
   "music"                 : "features/music.html",
   "maps"                  : "features/maps.html",
-  "localondon"            : "features/locallondon.html",
+  "locallondon"            : "features/locallondon.html",
   "surveys"               : "about/surveys.html" 
 };
